661009b48b642f59c38fbd5852bcac0f41ff86a2
Tamas Erdos <github@tamaserdos.com>
2020-03-03 21:24:30 +0200

    Updates

---
+++ precache-manifest.5f00ff9ee3a9e82cd0bdf030fd19045e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "600ef3b5aa630d4473bae05db280dbe0",
+    "url": "/cuber/index.html"
+  },
+  {
+    "revision": "cc6ae92e33dab16c4830",
+    "url": "/cuber/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8142ba671fbd0a09438a",
+    "url": "/cuber/static/js/2.dd866663.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cuber/static/js/2.dd866663.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6ae92e33dab16c4830",
+    "url": "/cuber/static/js/main.5339026c.chunk.js"
+  },
+  {
+    "revision": "50902d1d4d21e18431c3",
+    "url": "/cuber/static/js/runtime-main.d4ac12e0.js"
+  }
+]);
